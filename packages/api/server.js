dd5727bb5be3b28f1cf12e234a01d51e874a2bbc
JustinRoderick <justinroderick258@gmail.com>
2024-03-06 11:28:46 -0500

    API routes test

--- packages/api/server.js
+++ packages/api/server.js
@@ -3,6 +3,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+
+const users = require("./routes/users");
+
+users(app);
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -32,84 +37,3 @@ app.get("/", function (req, res, next) {
   res.send("Hello world");
 });
 
-// TODO: CHANGE TO PROJECT
-app.post("/api/login", async (req, res, next) => {
-   // incoming: username, password
-   // outgoing: id, firstName, lastName, , error
-   // TODO: Return Business id also
-
-   // Init. error var
-   var error = "";
-   const { username, password } = req.body;
-
-   // Connect to database
-   const db = client.db("inventory_tracker");
-
-   try {
-     // In users collection, find the username and password record that matches the incoming user and password
-     const results = await db
-       .collection("users")
-       .find({ username: username, password: password })
-       .toArray();
-
-     // instantiate variables to store the results found in the database that we want to send back(id, fn, and ln)
-     var id = -1;
-     var fn = "";
-     var ln = "";
-
-     // if results found, obtain from array and store in the init. variables
-     if (results.length > 0) {
-       id = results[0]._id;
-       fn = results[0].firstName;
-       ln = results[0].lastName;
-
-       // Return what we just stored in our vars, id, fn, ln
-       var ret = { _id: id, firstName: fn, lastName: ln, error: "" };
-       return res.status(200).json(ret);
-     }
-
-     // User not found
-     var ret = { error: "User not found/incorrect username or password" };
-     return res.status(401).json(ret);
-   } catch (error) {
-     console.error("Error during login:", error);
-     return res.status(500).json({ error: "Internal Server Error" });
-   }
- });
-
-// User registration
-app.post("/api/register", async (req, res, next) => {
-  const { firstName, lastName, username, password, email, businessIdList } =
-    req.body;
-  const usersCollection = client.db("inventory_tracker").collection("users");
-
-  try {
-    const newUser = await usersCollection.insertOne({
-      firstName: firstName,
-      lastName: lastName,
-      username: username,
-      password: password,
-      email: email,
-      businessIdList: businessIdList || [],
-    });
-
-    const createdUser = await usersCollection.findOne(
-      { _id: newUser.insertedId },
-      { projection: { _id: 0, firstName: 1, businessIdList: 1 } }
-    );
-
-    if (createdUser) {
-      res.status(201).json({ error: "" });
-    } else {
-      res.status(404).json({ error: "User not found after registration." });
-    }
-  } catch (e) {
-    if (e instanceof MongoServerError && e.code === 11000) {
-      const errorField = e.message.includes("email_1") ? "Email" : "Username";
-      return res.status(400).json({ error: `${errorField} Taken` });
-    }
-
-    console.log(e);
-    res.status(500).json({ error: "Server error" });
-  }
-});
---
+++ packages/api/src/router/users.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser")
+const cors = require("cors");
+
+router.post("/api/login", async (req, res, next) => {
+  var error = "";
+  const { username, password } = req.body;
+
+  // Connect to database
+  const db = client.db("inventory_tracker");
+
+  try {
+    // In users collection, find the username and password record that matches the incoming user and password
+    const results = await db
+      .collection("users")
+      .find({ username: username, password: password })
+      .toArray();
+
+    // instantiate variables to store the results found in the database that we want to send back(id, fn, and ln)
+    var id = -1;
+    var fn = "";
+    var ln = "";
+
+    // if results found, obtain from array and store in the init. variables
+    if (results.length > 0) {
+      id = results[0]._id;
+      fn = results[0].firstName;
+      ln = results[0].lastName;
+
+      // Return what we just stored in our vars, id, fn, ln
+      var ret = { _id: id, firstName: fn, lastName: ln, error: "" };
+      return res.status(200).json(ret);
+    }
+
+    // User not found
+    var ret = { error: "User not found/incorrect username or password" };
+    return res.status(401).json(ret);
+  } catch (error) {
+    console.error("Error during login:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// User registration
+router.post("/api/register", async (req, res, next) => {
+  const { firstName, lastName, username, password, email, businessIdList } =
+    req.body;
+  const usersCollection = client.db("inventory_tracker").collection("users");
+
+  try {
+    const newUser = await usersCollection.insertOne({
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      password: password,
+      email: email,
+      businessIdList: businessIdList || [],
+    });
+
+    const createdUser = await usersCollection.findOne(
+      { _id: newUser.insertedId },
+      { projection: { _id: 0, firstName: 1, businessIdList: 1 } }
+    );
+
+    if (createdUser) {
+      res.status(201).json({ error: "" });
+    } else {
+      res.status(404).json({ error: "User not found after registration." });
+    }
+  } catch (e) {
+    if (e instanceof MongoServerError && e.code === 11000) {
+      const errorField = e.message.includes("email_1") ? "Email" : "Username";
+      return res.status(400).json({ error: `${errorField} Taken` });
+    }
+
+    console.log(e);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+mudule.exports = function (app) {
+  app.use('/users', router);
+}
+
+
+
