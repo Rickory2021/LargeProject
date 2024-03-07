c1180ecd1c1c98f361f76ee010d024068e868eb6
Justin Roderick <justinroderick258@gmail.com>
2024-03-06 21:46:11 -0500

    Add default page and signin/signup buttons

---
+++ apps/nextjs/src/app/sign-in/page.js
@@ -0,0 +1,12 @@
+import { redirect } from "next/navigation";
+
+
+import SignIn from  "./sign-in"
+
+export default async function Morestuff(){
+    return(
+        <div>
+            <h1>Sign In</h1>
+        </div>
+    );
+}
