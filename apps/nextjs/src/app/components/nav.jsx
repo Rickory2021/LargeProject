1176d8f85d0ccfb1e5590adcf615094248203b4c
Justin Roderick <justinroderick258@gmail.com>
2024-03-07 08:24:39 -0500

    Add navbar to components

---
+++ apps/nextjs/src/app/components/nav.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+
+export function Nav() {
+  return (
+    <div classname="flex flex-1 flex-col justify-center">
+      <nav className="bg-gray-800 p-4">
+        <div className="max-w-7xl mx-auto flex justify-between items-center">
+          <div className="flex-shrink-0">
+            <a href="./" className="text-white text-xl font-bold">
+              Slicer
+            </a>
+          </div>
+          <div className="hidden md:block .justify-center ">
+            <Link href="/sign-in">
+              <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md">
+                SignIn
+              </button>
+            </Link>
+
+            <Link href="/sign-up">
+              <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md">
+                Signup
+              </button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
--- apps/nextjs/src/app/page.js
+++ apps/nextjs/src/app/page.js
@@ -1,35 +1,14 @@
 import Image from "next/image";
+import React from "react";
 import Link from "next/link";
 
 
-import { Navbar } from "./components/nav";
+import { Nav } from "./components/nav";
 
 
 export default function HomePage() {
   return (
-    <div classname="flex flex-1 flex-col justify-center">
-      <nav className="bg-gray-800 p-4">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
-          <div className="flex-shrink-0">
-            <a href="#" className="text-white text-xl font-bold">
-              Slicer
-            </a>
-          </div>
-          <div className="hidden md:block .justify-center ">
-            <Link href="/sign-in">
-              <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md">
-                SignIn
-              </button>
-            </Link>
-
-            <Link href="/sign-up">
-              <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md">
-                Signup
-              </button>
-            </Link>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <Nav />
+    
   );
 }
