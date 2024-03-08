1647b2b0634536ca8b8b91e0feeeb641252fdb3b
Justin Roderick <justinroderick258@gmail.com>
2024-03-08 14:43:40 -0500

    Fix signin/register

--- apps/nextjs/src/app/sign-up/sign-up.js
+++ apps/nextjs/src/app/sign-up/sign-up.js
@@ -17,7 +17,7 @@ export default function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/register", {
+      const res = await fetch("http://localhost:3001/api/user/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
