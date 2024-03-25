6b4ab25b36a4d04f6cea1f712e63f1edcba13b0d
Karina Yuja <66886321+KariLinux@users.noreply.github.com>
2024-03-25 16:34:30 -0400

    add routes for remove connection and get business name endpoints

--- packages/api/routes/auth_route.js
+++ packages/api/routes/auth_route.js
@@ -1,11 +1,15 @@
-const { Signup } = require('../controllers/user_auth_controller');
-const { Login } = require('../controllers/user_auth_controller');
-const { Logout } = require('../controllers/user_auth_controller');
-const { RegisterBusiness } = require('../controllers/business_auth_controller');
+const {
+  Signup,
+  Login,
+  Logout
+} = require('../controllers/user_auth_controller');
+const {
+  RegisterBusiness,
+  AddUserBusinessConn,
+  RemoveUserBusinessConn,
+  GetBusinessName
+} = require('../controllers/business_auth_controller');
 const { userVerification } = require('../middlewares/auth_middleware');
-// TODO: Uncomment when making these endpoints
-// const { AddUserBusinessConn } = require("../Controllers/BusinessAuthController");
-// const { RemoveUserBusinessConn } = require("../Controllers/BusinessAuthController");
 const router = require('express').Router();
 
 // User routes
@@ -18,8 +22,8 @@ router.post('/', userVerification);
 
 // Business routes
 router.post('/business/register', RegisterBusiness);
-// TODO: Uncomment when making these endpoints
-// router.post('/business/addConnection', AddUserBusinessConn);
-// router.post('/business/removeConnection', RemoveUserBusinessConn);
+router.post('/business/addConnection', AddUserBusinessConn);
+router.post('/business/removeConnection', RemoveUserBusinessConn);
+router.post('/business/getName', GetBusinessName);
 
 module.exports = router;
