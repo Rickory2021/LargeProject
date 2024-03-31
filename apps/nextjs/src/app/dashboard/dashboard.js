88f55e6ce1f57353bd68fd3779d49c43093ca339
TrystenElson <98505423+TrystenElson@users.noreply.github.com>
2024-03-31 13:25:30 -0400

    Created Cookie Component to verify the Access Token and return the userId.
    See dashboard for example usage.

--- apps/nextjs/src/app/dashboard/dashboard.js
+++ apps/nextjs/src/app/dashboard/dashboard.js
@@ -1,8 +1,23 @@
+'use client';
 import { SideNav } from '@repo/ui/side-nav';
+import CookieComponent from '../components/CookieComponent';
+import React from 'react';
+import { useState } from 'react';
+const userId = <CookieComponent cookieName={'accessToken'} />;
 
 export function Dashboard() {
+  const [userId, setUserId] = useState(null);
+
+  // Function to handle userId change
+  const handleUserIdChange = userId => {
+    setUserId(userId);
+  };
   return (
-    <div className="flex felx-col">
+    <div className="flex flex-col">
+      <CookieComponent
+        cookieName={'accessToken'}
+        onUserIdChange={handleUserIdChange}
+      />
       <SideNav />
     </div>
   );
