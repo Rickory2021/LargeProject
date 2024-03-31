60c44b4c1f1916654f835d4222b57e372b1406bf
Rickory2021 <rickylego15@gmail.com>
2024-03-31 19:05:53 -0400

    Edit & Checks for

--- packages/api/routes/crud_route.js
+++ packages/api/routes/crud_route.js
@@ -5,19 +5,21 @@ const {
 } = require('../controllers/crud/business_controller');
 const {
   createItem,
-  readItem,
+  readAllItemName,
+  readOneItem,
   updateItem,
   deleteItem
-} = require('../controllers/crud/item_list_controller');
+} = require('../controllers/crud/item_controller');
 
 // Employee Id List Route /crud
 router.get('/business/employee-id-list', ReadEmployeeIdList); // /api/auth/business/employeeIdList?id
 
 // Item List Route /crud
 
-router.post('/business/item-list/create', createItem); // ?businessId&printedFieldNameList
-router.post('/business/item-list/read', readItem); // ?businessId&printedFieldNameList
-router.post('/business/item-list/update', updateItem); // ?businessId&identityField&identityValue&editField&editValue
-router.post('/business/item-list/delete', deleteItem); // ?businessId&identityField&identityValue
+router.post('/business/item-list/create', createItem); // ?businessId&itemName
+router.post('/business/item-list/read-all', readAllItemName); // ?businessId
+router.post('/business/item-list/read-one', readOneItem); // ?businessId&itemName
+router.post('/business/item-list/update', updateItem); // ?businessId&findItemName&newItemName
+router.post('/business/item-list/delete', deleteItem); // ?businessId&&itemName
 
 module.exports = router;
