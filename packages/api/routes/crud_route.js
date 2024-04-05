69ab61bef5f01cd664e0d82f9be0250df480d31b
Rickory2021 <rickylego15@gmail.com>
2024-04-05 17:27:10 -0400

    Finish portionInfo along with updated edits for item-list

--- packages/api/routes/crud_route.js
+++ packages/api/routes/crud_route.js
@@ -13,6 +13,14 @@ const {
 } = require('../controllers/crud/item_controller');
 
 const {
+  createPortionInfo,
+  readAllPortionInfo,
+  updatePortionInfoName,
+  updatePortionInfoNumber,
+  deletePortionInfo
+} = require('../controllers/crud/portion_info_controller');
+
+const {
   addDistributorMetaData,
   readDistributorMetaData,
   updateDistributorMetaData,
@@ -23,11 +31,21 @@ const {
 router.get('/business/employee-id-list/read-all', readAllEmployeeIds); //GET /api/crud/business/employee-id-list/read-all?businessId=
 
 // Item List Route /crud
-router.post('/business/item-list/create', createItem); // ?businessId&itemName
+router.post('/business/item-list/create', createItem); // ?businessId {itemName}
 router.post('/business/item-list/read-all', readAllItemName); // ?businessId
-router.post('/business/item-list/read-one', readOneItem); // ?businessId&itemName
-router.post('/business/item-list/update', updateItem); // ?businessId&findItemName&newItemName
-router.post('/business/item-list/delete', deleteItem); // ?businessId&&itemName
+router.post('/business/item-list/read-one', readOneItem); // ?businessId {itemName}
+router.post('/business/item-list/update', updateItem); // ?businessId {findItemName, newItemName}
+router.post('/business/item-list/delete', deleteItem); // ?businessId {itemName}
+
+// Portion List Infor
+router.post('/business/portion-info-list/create', createPortionInfo); // ?businessId&itemName {unitName,unitNumber}
+router.post('/business/portion-info-list/read-all', readAllPortionInfo); // ?businessId  {itemName}
+router.post('/business/portion-info-list/update-name', updatePortionInfoName); // ?businessId&itemName {findUnitName,c}
+router.post(
+  '/business/portion-info-list/update-number',
+  updatePortionInfoNumber
+); // ?businessId&findItemName&newItemName
+router.post('/business/portion-info-list/delete', deletePortionInfo); // ?businessId&&itemName {unitName}
 
 // Distributor metadata
 router.post('/business/distributor-metadata-list/add', addDistributorMetaData);
