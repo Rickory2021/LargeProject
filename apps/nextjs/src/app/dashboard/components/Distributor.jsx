75261f638351bd3a36e0301d95e83d4a1ff6baba
TrystenElson <98505423+TrystenElson@users.noreply.github.com>
2024-04-15 02:37:49 -0400

    Distributor page functionality done.

---
+++ apps/nextjs/src/app/dashboard/components/Distributor.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+function Distributor({ itemName, businessId, updateDistributorList }) {
+  const [loading, setLoading] = useState(true);
+
+  const requestBody = {
+    itemName: itemName
+  };
+
+  useEffect(() => {
+    const getDistributorList = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3001/api/crud/business/distributor-item/read-all?businessId=' +
+            businessId,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch item names');
+        }
+        const data = await response.json();
+        const outputList = data.outputList;
+        const distributorNames = outputList
+          .map(item => item.distributorItemList)
+          .flat();
+        updateDistributorList(distributorNames);
+      } catch (error) {
+        console.error('Error fetching Distributor List:', error);
+      } finally {
+        setLoading(false); // Update loading status when operation completes
+      }
+    };
+
+    if (itemName && businessId) {
+      getDistributorList();
+    }
+  }, []); // Add dependencies for useEffect
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return null;
+}
+
+export default Distributor;
