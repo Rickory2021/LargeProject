4a5f3b5d8cc30537f22cd0b0a83af28bd7bc41eb
Your Name <you@example.com>
2024-04-18 05:31:56 -0400

    confirm password

--- apps/nextjs/src/app/dashboard/components/side-nav.jsx
+++ apps/nextjs/src/app/dashboard/components/side-nav.jsx
@@ -113,11 +113,15 @@ const SideNav = ({ openCallback }) => {
                     <Link href="/dashboard">
                       {menu.icon ? menu.icon : <FaHome />}
                     </Link>
+                  ) : menu.title === 'Update' ? (
+                    <Link href="/dashboard/update-by-item">
+                      {menu.icon ? menu.icon : <FaHome />}
+                    </Link>
                   ) : (
                     <Link href={`/dashboard/${menu.title.toLowerCase()}`}>
                       {menu.icon ? menu.icon : <FaHome />}
                     </Link>
-                  )}
+                  ) }
                 </span>
 
                 <span
